4473b3f73fff38305ce234a97f1fc08c50dbf72a
ddongree <sdfzl_ar@naver.com>
2023-06-10 17:16:01 +0900

    Component 추출 전 상태

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import Composition from './components/2-4.Props/Composition';
 import './App.css';
+import Extraction from './components/2-4.Props/Extraction/Extraction';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Composition />
+      <Extraction />
     </div>
   );
 }
